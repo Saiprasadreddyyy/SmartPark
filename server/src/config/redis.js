443016fe38f45636b01353bcbce1a27416124e5e
Saiprasad <saiprasad3969@gmail.com>
2025-12-02 12:56:36 +0530

    redis connection

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -2,7 +2,8 @@ import Redis from "ioredis";
 
 const redis = new Redis({
   host: process.env.REDIS_HOST || "localhost",
-  port: process.env.REDIS_PORT || 6379,
+  port: process.env.REDIS_PORT || 63712,  // double-check your port number
+  tls: {},  // RedisLabs cloud requires TLS even without password
   retryStrategy: (times) => Math.min(times * 50, 2000)
 });
 
